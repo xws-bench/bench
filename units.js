2ca678cc2012ee928a0cb57b506ccf5ceb32ec5d
DenisWings <deniswings@free.fr>
2015-07-24 01:21:04 +0200

    iaunit and show

--- units.js
+++ units.js
@@ -1504,6 +1504,7 @@ Unit.prototype = {
     },
     endaction: function() {
 	this.actiondone=true; this.action=-1;
+	this.show();
 	//console.log("endaction "+this.name+">nextstep");
 	nextstep();
 	//console.log("endaction "+this.name+"<nextstep");
