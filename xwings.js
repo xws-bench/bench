42dacb6a1a62cd34859250321468a5c645eb0d4d
xws-bench <denis2@soult.local>
2015-09-20 00:08:06 +0200

    Try target for touch

--- xwings.js
+++ xwings.js
@@ -1293,6 +1293,7 @@ $(document).ready(function() {
 	mc.get("pinch").set({enable:true});
 	mc.get('pan').set({direction:Hammer.DIRECTION_ALL});
 	mc.on("panleft panright panup pandown",function(ev) {
+	    log(ev.target);
 	    if (activeunit.dragged==true) return;
 	    viewport_translate(-ev.velocityX*50,-ev.velocityY*50);
 	});
