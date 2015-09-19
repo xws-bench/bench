1a2416a1f4ddadb5e57fb81c33853e2985d6b2a2
xws-bench <denis2@soult.local>
2015-09-19 15:18:47 +0200

    Zoom

--- xwings.js
+++ xwings.js
@@ -1303,7 +1303,7 @@ $(document).ready(function() {
 	    var x=vm.x(ev.center.x,ev.center.y);
 	    var y=vm.y(ev.center.x,ev.center.y);
 	    mc.zoom=ev.scale/mc.zoom;
-	    VIEWPORT.m.translate(x,y).scale(mc.zoom).translate(-x,-y);
+	    //VIEWPORT.m.translate(x,y).scale(mc.zoom).translate(-x,-y);
 	    VIEWPORT.transform(VIEWPORT.m);
 	    activeunit.show();
 	    if (ev.final) mc.zoom=1;
