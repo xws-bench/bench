bb3e0422477008dedc3033d22c69514ec133bcb7
DenisWings <deniswings@free.fr>
2015-09-20 00:12:34 +0200

    Id of target

--- xwings.js
+++ xwings.js
@@ -1293,7 +1293,7 @@ $(document).ready(function() {
 	mc.get("pinch").set({enable:true});
 	mc.get('pan').set({direction:Hammer.DIRECTION_ALL});
 	mc.on("panleft panright panup pandown",function(ev) {
-	    log(ev.target);
+	    log(ev.target.id);
 	    if (activeunit.dragged==true) return;
 	    viewport_translate(-ev.velocityX*50,-ev.velocityY*50);
 	});
