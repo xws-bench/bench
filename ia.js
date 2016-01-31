9b9278f3d0cfe587eda494af7ebef0bf18e74e0d
DenisWings <deniswings@free.fr>
2016-01-31 16:01:12 +0100

    Deleted unused files

--- ia.js
+++
@@ -1,7 +0,0 @@
-importScripts("obstacles.js","critical.js","units.js","iaunits.js","pilots.js","upgrades.js","team.js");
-onmessage = function(e) {
-    if (e.data[0]=="SQUADRON") {
-	console.log('Posting message back to main script');
-	postMessage(e.data[1]);
-    }
-}
